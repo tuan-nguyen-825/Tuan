6f9b915edc4b4ae223c56a191bce0879dfd7cf72
Toshiba_Radius\toshiba <tuan.ng.825@gmail.com>
2022-03-16 23:57:37 -0400

    no message

--- Module 5 Assignment/src/FibRecursion.java
+++ Module 5 Assignment/src/FibRecursion.java
@@ -1,8 +1,18 @@
-
+/**
+ * Class to find a Fibonacci number using recursion method.
+ * 
+ * @author Tuan Nguyen
+ *
+ */
 public class FibRecursion {
 	//Declare variable
 	int fib;
-	//Find Fibonacci using recursion method
+	/**
+	 * Method to find Fibonacci number using recursion approch
+	 * 
+	 * @param fib fibonacci number at a given nth nubmer
+	 * @return fib
+	 */
 	public int getFibNumber(int n)
 	{
 		if (n == 1) return 0;
