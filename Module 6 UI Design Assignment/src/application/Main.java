aee04dd00ba20b0bfc67babb05281264e8bce4bf
Toshiba_Radius\toshiba <tuan.ng.825@gmail.com>
2022-03-24 11:35:14 -0400

    no message

--- Module 6 UI Design Assignment/src/application/Main.java
+++ Module 6 UI Design Assignment/src/application/Main.java
@@ -1,10 +1,12 @@
 package application;
-	
+
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
@@ -14,8 +16,10 @@ public class Main extends Application {
 			Parent root = FXMLLoader.load(getClass().getResource("/MainScene.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//scene.setFill(Color.BLUE);
+		
 			
-			
+			primaryStage.setTitle("Word Occurrences Application");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- Module 6 UI Design Assignment/src/application/MainSceneController.java
+++ Module 6 UI Design Assignment/src/application/MainSceneController.java
@@ -1,13 +1,99 @@
 package application;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 
 public class MainSceneController {
-	public void up(ActionEvent e) {
-		System.out.println("up");
-	}
+	@FXML private Label labelResult;
+	@FXML private TextField textFieldFileName;
 	
-	public void down(ActionEvent e) {
-		System.out.println("down");
+//	@Override 
+//	public void initialize(URL url, ResourceBundle r) {
+//		System.out.println("Hi");
+//	}
+
+	// Read a file
+	public void readFile(ActionEvent event) {
+		try {
+//			Scanner scan = new Scanner(System.in);
+//			System.out.println("Enter a file name you want to open: ");
+			
+//			String fileName = scan.nextLine();
+			String fileName = textFieldFileName.getText();
+
+			// Open file
+			File file = new File(fileName);
+
+			// Read file
+			Scanner fileReader = new Scanner(file);
+			while(fileReader.hasNextLine()) {
+				String data = fileReader.nextLine();
+				System.out.println(data);
+			}
+			fileReader.close();//After complete scanning, close the file
+		} catch (Exception e) {
+			System.out.println("Invalid file Or something wrong.");
+		}
+	}// end readFile
+	
+	// Write to a file
+	public void writeFile(ActionEvent event) {
+		try {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Enter a file name you want to write into it: ");
+			String fileName = scan.nextLine();
+			System.out.println("Enter your text: ");
+			String fileContent = scan.nextLine();
+			
+			FileWriter fileWriter = new FileWriter(fileName);
+			fileWriter.write(fileContent);
+			fileWriter.close();
+			
+			System.out.println("Your text has successful added to the file: ");
+		}
+		catch (Exception e) {
+			System.out.println("Error!!!");
+			e.printStackTrace();
+		}
+	}//end
+	
+	// Count word in a file
+	public void countWord(ActionEvent event) {
+		
+		try {
+			int wordCount = 0;
+			
+			Scanner scan = new Scanner(System.in);
+//			System.out.println("Enter a file name you want to count: ");
+//			String fileName = scan.nextLine();
+			
+//			File file = new File(fileName);
+			File file = new File(textFieldFileName.getText());
+
+			scan = new Scanner (file);
+			while (scan.hasNext()) {
+				String word = scan.next();
+				wordCount++;
+			}
+			scan.close();
+			System.out.print("Number of word in your file is: ");
+			System.out.println(wordCount);
+			labelResult.setText(String.valueOf(wordCount)+" words");
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File is not existing. Please check file name again.");
+			e.printStackTrace();
+		}
+		
 	}
+
+
 }
+	
