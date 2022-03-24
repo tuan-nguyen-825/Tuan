0f8c12e01ec2797cf8fd5b1fa5116c28b3d9fdc3
Toshiba_Radius\toshiba <tuan.ng.825@gmail.com>
2022-03-23 23:00:21 -0400

    set up scene builder. Done

---
+++ Module 6 UI Design Assignment/src/application/MainSceneController.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class MainSceneController {
+	public void up(ActionEvent e) {
+		System.out.println("up");
+	}
+	
+	public void down(ActionEvent e) {
+		System.out.println("down");
+	}
+}
